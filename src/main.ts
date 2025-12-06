a7105e2e52938b7f7fe4eae3f3ca5512896e381f
taleeby <37315048+taleeby@users.noreply.github.com>
2025-12-07 00:15:18 +1100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Authenticator } from '@aws-amplify/ui-react';
 
 
 
